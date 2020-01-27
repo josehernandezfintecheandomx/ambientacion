8750fea534a3e74c2a6f05ca003a7da28fb42a26
Alberto <jose.hernandez@fintecheando.mx>
2020-01-27 13:50:06 -0600

    Install PostgreSQL

---
+++ scripts/install-postgresql.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get update
+
+sudo apt install postgresql postgresql-contrib
+
+# sudo -i -u postgres
+# psql
+
+# sudo -u postgres psql
+
+# Crear usuario 
+# sudo -u postgres createuser --interactive
+
+# Crear Base de Datos
+# sudo -u postgres createdb BASE_DE_DATOS
